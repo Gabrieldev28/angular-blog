32d82cecc3658e6ad025e7e17fdd36bfdd21175a
Gabsdev28 <gabrieldev28@gmail.com>
2023-10-26 00:26:00 -0300

    components angularDIO finish

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,24 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Homem de ferro esta nos cinemas",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor enim labore optio itaque voluptatem, exercitationem vitae at totam incidunt, natus assumenda. Nemo sit veniam sint id adipisci voluptatem Repellat ratione fuga quas mollitia perspiciatis quidem cum dolor explicabo?",
+        "photoCover":"https://t.ctcdn.com.br/bBqQ0j6FNPyDxr05w8QkhuwpHI8=/768x432/smart/i798787.jpeg"
+    },
+    {
+        "id":"2",
+        "title": "Novo filme do Thor",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor enim labore optio itaque voluptatem, exercitationem vitae at totam incidunt, natus assumenda. Nemo sit veniam sint id adipisci voluptatem Repellat ratione fuga quas mollitia perspiciatis quidem cum dolor explicabo?",
+        "photoCover":"https://classic.exame.com/wp-content/uploads/2018/10/thor-ragnarok-filme-cultura-vip.jpg?quality=70&strip=info&w=720"
+    },{
+        "id":"3",
+        "title": "Spider-man: No way home",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor enim labore optio itaque voluptatem, exercitationem vitae at totam incidunt, natus assumenda. Nemo sit veniam sint id adipisci voluptatem Repellat ratione fuga quas mollitia perspiciatis quidem cum dolor explicabo?",
+        "photoCover":"https://topmovies.com.br/wp-content/uploads/2023/08/No-Way-Home-Concept-Art-Shows-Doutor-Estranho-Une-o.jpg.webp"
+    },{
+        "id":"4",
+        "title": "Melhores Cenas do Homem Formiga",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor enim labore optio itaque voluptatem, exercitationem vitae at totam incidunt, natus assumenda. Nemo sit veniam sint id adipisci voluptatem Repellat ratione fuga quas mollitia perspiciatis quidem cum dolor explicabo?",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/57DB2EF2BF00E176695BF156D65AF178A5C368136F5779243C194CE437185386/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+]
